39ad7acdbbcc1032e2ee86daaf7e5c35e6a7c343
Trini Fang <yuhsun.fang@esmt.berlin>
2026-03-12 15:04:57 +0100

    Fix syntax error in hello.py

--- hello.py
+++ hello.py
@@ -2,4 +2,4 @@ print("Welcome to MAAI Workshop")
 print("Please write your name here\n")
 #Each teammate should enter their name individually and push the update to GitHub
 print("Pascal")
-print("Trini"
+print("Trini")
