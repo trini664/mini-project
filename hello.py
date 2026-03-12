b7d12b24344541ee2a7b57fccae4664609554c29
Trini Fang <yuhsun.fang@esmt.berlin>
2026-03-12 15:08:46 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 print("Welcome to MAAI Workshop")
-print("Please write your name here\n")
+print("Please write your name here: Trini")
 #Each teammate should enter their name individually and push the update to GitHub
 print("Pascal")
